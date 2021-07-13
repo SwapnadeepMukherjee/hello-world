ebc66c37115b9a553aadb1a030b41a27fc0880f1
Swapnadeep Mukherjee <swapnadeepm@gmail.com>
2021-07-14 00:17:36 +0530

    Water

---
+++ hungry.py
@@ -0,0 +1,10 @@
+hungry=input("Are you hungry")
+if hungry=="yes':
+    print("Eat Samosa")
+    print("Eat Pizza")
+    print("Eat Burger")
+else:
+    thristy=input("Are you thristy")
+    if hungry=="yes':
+        print("Drink Water")
+        print("Drink Cold-Drinks")
