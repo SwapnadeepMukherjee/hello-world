f55a26211a060e7288bd0084567857d3a128df88
Swapnadeep Mukherjee <swapnadeepm@gmail.com>
2021-09-04 21:44:39 +0530

    Adding updated logic file

--- hungry.py
+++ hungry.py
@@ -1,11 +1,11 @@
 hungry=input("Are you hungry")
-if hungry=="yes':
+if hungry=="yes":
     print("Eat Samosa")
     print("Eat Pizza")
     print("Eat Burger")
 else:
     thristy=input("Are you thristy")
-    if hungry=="yes':
+    if hungry=="yes":
         print("Drink Water")
         print("Drink Cold-Drinks")
         print("Drink Soda")
